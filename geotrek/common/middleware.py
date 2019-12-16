99c75f79a34f641bd909c8c9b0a7824f2dd67452
Bastien Potiron <merbatop@courriel.fr.nf>
2019-12-16 17:55:10 +0100

    [Migration Django 2] [UPD] change methods to propertie
    Using User.is_authenticated() and User.is_anonymous() as methods rather than properties is no longer supported.

--- geotrek/common/middleware.py
+++ geotrek/common/middleware.py
@@ -95,7 +95,7 @@ class FixedAutoLoginMiddleware(AutoLoginMiddleware):
 
         user = getattr(request, 'user', None)
 
-        if user and user.is_anonymous() and not is_running_tests:
+        if user and user.is_anonymous and not is_running_tests:
             remoteip = request.META.get('REMOTE_ADDR')
             remotehost = request.META.get('REMOTE_HOST')
 
